d846142f516a661bb6e8367ed0e545f93ce9aca5
Lucas Lopes <medowlucas@gmail.com>
2022-01-29 23:19:57 -0300

    finalizando a comunicação entre as classes Dev, Bootcamp e executando programa principal

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -22,9 +24,31 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria de Java");
         mentoria.setData(LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas");
+        System.out.println("---Cursos do Lucas---");
+        devLucas.inscreverBootcamp(bootcamp);
+        devLucas.progredir();
+        devLucas.progredir();
+        System.out.println("Xp Lucas " + devLucas.calcularXp());
+        System.out.println("Conteúdos Inscritos" + devLucas.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devLucas.getConteudosConcluidos());
+
+        Dev devJoao = new Dev();
+        System.out.println("---Cursos do João---");
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        devJoao.progredir();
+        System.out.println("Xp Joãp " + devJoao.calcularXp());
+        System.out.println("Conteúdos Inscritos" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devJoao.getConteudosConcluidos());
+
     }
 }
